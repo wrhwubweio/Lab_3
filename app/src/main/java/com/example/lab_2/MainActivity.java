bd0a4dad0c6a8e5cee8e4558bda4a519d0869e54
Ivan <i.sh.911@ya.ru>
2023-02-24 20:29:40 +0300

    Работа с activity

--- app/src/main/java/com/example/lab_2/MainActivity.java
+++ app/src/main/java/com/example/lab_2/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.lab_2;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button input;
+    private TextView textView;
+    private EditText editText;
+    private String TAG = "MyTag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.relative_layout);
+        setContentView(R.layout.constraint_layout);
+        Button input = (Button) findViewById(R.id.ok);
+        TextView textView = (TextView) findViewById(R.id.outText);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        //textView.setText(editText.getText());
+        input.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                intent.putExtra("text",  String.valueOf(editText.getText()));
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/lab_2/SecondActivity.java
@@ -0,0 +1,30 @@
+package com.example.lab_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear_layout);
+        TextView textView = (TextView) findViewById(R.id.outText);
+        textView.setText(getIntent().getStringExtra("text"));
+        Button input = (Button) findViewById(R.id.ok);
+        input.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
